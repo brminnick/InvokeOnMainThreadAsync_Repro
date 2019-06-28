417547e496091a31218b90df3d0223182a65525b
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-06-28 14:04:35 -0700

    Added Crash2

--- InvokeOnMainThreadAsync_Repro/App.cs
+++ InvokeOnMainThreadAsync_Repro/App.cs
@@ -12,11 +12,14 @@ namespace InvokeOnMainThreadAsync_Repro
     {
         public MainPage()
         {
-            var animatedButton_Crash = new Button { Text = "Crash" };
-            animatedButton_Crash.Clicked += AnimatedButtonCrash_Clicked;
-
             var animatedButton_NoCrash = new Button { Text = "No Crash" };
             animatedButton_NoCrash.Clicked += AnimatedButtonNoCrash_Clicked;
+
+            var animatedButton_Crash1 = new Button { Text = "Crash1" };
+            animatedButton_Crash1.Clicked += AnimatedButtonCrash1_Clicked;
+
+            var animatedButton_Crash2 = new Button { Text = "Crash2" };
+            animatedButton_Crash2.Clicked += AnimatedButtonCrash2_Clicked;
 
             Content = new StackLayout
             {
@@ -24,25 +27,22 @@ namespace InvokeOnMainThreadAsync_Repro
                 VerticalOptions = LayoutOptions.Center,
                 Children =
                 {
-                    animatedButton_Crash,
-                    animatedButton_NoCrash
+                    animatedButton_NoCrash,
+                    animatedButton_Crash1,
+                    animatedButton_Crash2
                 }
             };
         }
 
-        async void AnimatedButtonCrash_Clicked(object sender, System.EventArgs e) => await AnimateButton_Crash((Button)sender);
+        async void AnimatedButtonCrash1_Clicked(object sender, System.EventArgs e) => await AnimateButton_Crash1((Button)sender);
+
+        async void AnimatedButtonCrash2_Clicked(object sender, System.EventArgs e) => await AnimateButton_Crash2((Button)sender);
 
         async void AnimatedButtonNoCrash_Clicked(object sender, System.EventArgs e) => await AnimateButton_NoCrash((Button)sender);
 
-        Task AnimateButton_Crash(View view)
-        {
-            return Device.InvokeOnMainThreadAsync(async () =>
-            {
-                await view.RotateTo(320);
-                await view.RotateTo(-320);
-                await view.RotateTo(0);
-            });
-        }
+        Task AnimateButton_Crash1(View view) => Device.InvokeOnMainThreadAsync(async () => await RotateView(view).ConfigureAwait(false));
+
+        Task AnimateButton_Crash2(View view) => Device.InvokeOnMainThreadAsync(() => RotateView(view));
 
         Task AnimateButton_NoCrash(View view)
         {
@@ -50,14 +50,19 @@ namespace InvokeOnMainThreadAsync_Repro
 
             Device.BeginInvokeOnMainThread(async () =>
             {
-                await view.RotateTo(320);
-                await view.RotateTo(-320);
-                await view.RotateTo(0);
+                await RotateView(view).ConfigureAwait(false);
 
                 tcs.SetResult(null);
             });
 
             return tcs.Task;
+        }
+
+        async Task RotateView(View view)
+        {
+            await view.RotateTo(320);
+            await view.RotateTo(-320);
+            await view.RotateTo(0);
         }
     }
 }
