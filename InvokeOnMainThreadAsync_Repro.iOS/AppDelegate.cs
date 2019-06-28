0c5ecede7a87b3ca8a7d9cd2f75314982a78562b
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-06-28 14:48:55 -0700

    Removed Xamarin.Essentials
    Essentials is unused in the crash repro

--- InvokeOnMainThreadAsync_Repro.iOS/AppDelegate.cs
+++ InvokeOnMainThreadAsync_Repro.iOS/AppDelegate.cs
@@ -6,12 +6,12 @@ namespace InvokeOnMainThreadAsync_Repro.iOS
     [Register(nameof(AppDelegate))]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
-        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        public override bool FinishedLaunching(UIApplication uiApplication, NSDictionary launchOptions)
         {
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
-            return base.FinishedLaunching(app, options);
+            return base.FinishedLaunching(uiApplication, launchOptions);
         }
     }
 }
